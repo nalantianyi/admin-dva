ad033f3a7c4060b236e25edcd82b6ea789f7bee3
纳兰天忆 <103520146@qq.com>
2017-03-21 11:02:26 +0800

    axios作为fetch的lib

--- web/services/app.js
+++ web/services/app.js
@@ -1,13 +1,13 @@
 /**
  * Created by nalantianyi on 2017/3/21.
  */
-import axios from 'axios';
+import request from '../utils/request';
 export async function login(params) {
-    return axios('/api/login', {method: 'POST', data: params});
+    return request('/api/login', {method: 'POST', data: params});
 }
 export async function logout(params) {
-    return axios('/api/logout', {method: 'POST', data: params});
+    return request('/api/logout', {method: 'POST', data: params});
 }
 export async function userInfo(params) {
-    return axios('/api/userInfo', {method: 'GET', data: params});
+    return request('/api/userInfo', {method: 'GET', data: params});
 }
---
+++ web/utils/request.js
@@ -0,0 +1,35 @@
+/**
+ * Created by nalantianyi on 2017/3/21.
+ */
+import axios from 'axios';
+
+function checkStatus(response) {
+    if (response.status >= 200 && response.status < 300) {
+        return response;
+    }
+
+    const error = new Error(response.statusText);
+    error.response = response;
+    throw error;
+}
+
+/**
+ * Requests a URL, returning a promise.
+ *
+ * @param  {string} url       The URL we want to request
+ * @param  {object} [options] The options we want to pass to "fetch"
+ * @return {object}           An object containing either "data" or "err"
+ */
+export default async function request(url, options) {
+    const response = await axios(url, options);
+
+    checkStatus(response);
+
+    const data = await response.json();
+
+    const ret = {
+        data
+    };
+
+    return ret;
+}
