e9d4c8bdae06ea363f87de6ad6147fab2edd7bcc
纳兰天忆 <103520146@qq.com>
2017-03-21 09:56:47 +0800

    init

--- web/index.js
+++ web/index.js
@@ -2,8 +2,15 @@
  * Created by nalantianyi on 2017/3/20.
  */
 import dva from 'dva';
-
+import createLoading from 'dva-loading';
+import {hashHistory} from'dva/router';
 console.log('a');
-const app = dva();
+const app = dva({
+    ...createLoading(),
+    history: hashHistory,
+    onError(error){
+        console.error('app onError -- ', error);
+    }
+});
 
 app.start('#root');
