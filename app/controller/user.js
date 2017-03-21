eb80e6f57b7b17f9b0753d3929f100879edc1b6a
纳兰天忆 <103520146@qq.com>
2017-03-21 17:02:26 +0800

    构建routes dashboard

---
+++ app/controller/user.js
@@ -0,0 +1,8 @@
+/**
+ * Created by nalantianyi on 2017/3/21.
+ */
+module.exports = app => {
+    class UserController extends app.Controller {
+    }
+    return UserController;
+};
---
+++ web/routes/dashboard.js
@@ -0,0 +1,19 @@
+/**
+ * Created by nalantianyi on 2017/3/21.
+ */
+import React, {PropTypes} from 'react';
+import {connect} from 'dva';
+import {color} from '../utils/theme';
+
+const bodyStyle = {
+    bodyStyle: {
+        height: 432,
+        background: '#fff'
+    }
+};
+function Dashboard() {
+}
+Dashboard.propTypes = {
+    dashboard: PropTypes.object
+};
+export default connect(({dashboard}) => ({dashboard}))(Dashboard);
