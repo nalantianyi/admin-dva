b7eaaa69d24fad8dfbc593b8e0d5afcc95fbdb07
纳兰天忆 <103520146@qq.com>
2017-03-21 11:07:40 +0800

    webpack添加less-loader

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -41,6 +41,17 @@ module.exports = {
                 exclude: /node_modules/
             },
             {
+                test: /\.less$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "less-loader" // compiles Less to CSS
+                }]
+            },
+            {
                 test: /\.css$/,
                 include: /node_modules/,
                 use: extractAntd.extract(['css-loader', 'postcss-loader' ]),
