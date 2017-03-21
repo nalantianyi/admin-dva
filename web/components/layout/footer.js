36b60c41f4b1ce820ad5d92f1383097ea80b6caf
纳兰天忆 <103520146@qq.com>
2017-03-21 12:50:34 +0800

    footer

---
+++ web/components/layout/footer.js
@@ -0,0 +1,10 @@
+/**
+ * Created by nalantianyi on 2017/3/21.
+ */
+import React from 'react';
+import styles from './main.less';
+import config from '../../utils/config';
+const Footer = () => <div className={styles.footer}>
+    {config.footerText}
+</div>
+export default Footer;
---
+++ web/utils/config.js
@@ -0,0 +1,11 @@
+/**
+ * Created by nalantianyi on 2017/3/21.
+ */
+export default {
+    name: 'Ant Design Admin',
+    prefix: 'antdAdmin',
+    footerText: 'Ant Design Admin 版权所有 © 2017 由 nalantianyi 支持',
+    logoSrc: 'https://t.alipayobjects.com/images/rmsweb/T1B9hfXcdvXXXXXXXX.svg',
+    logoText: 'Antd Admin',
+    needLogin: true,
+};
