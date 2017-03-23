580bf684f191ba8134e353146724b5a994aef43c
纳兰天忆 <103520146@qq.com>
2017-03-23 11:02:28 +0800

    app model

--- web/models/app.js
+++ web/models/app.js
@@ -11,18 +11,62 @@ export default {
         user: {
             name: '纳兰天忆'
         },
-        loginButtonLoading:false,
-        menuPopoverVisible:false,
-        siderFold:localStorage.getItem('antdAdminSiderFold')==='true',
-        darkTheme:localStorage.getItem('antdAdminDarkTheme')!=='false',
-        isNavbar:document.body.clientWidth<769,
-        navOpenKeys:JSON.parse(localStorage.getItem('navOpenKeys')||'[]')
+        loginButtonLoading: false,
+        menuPopoverVisible: false,
+        siderFold: localStorage.getItem('antdAdminSiderFold') === 'true',
+        darkTheme: localStorage.getItem('antdAdminDarkTheme') !== 'false',
+        isNavbar: document.body.clientWidth < 769,
+        navOpenKeys: JSON.parse(localStorage.getItem('navOpenKeys') || '[]')
     },
-    subscriptions:{
-        setup ({ dispatch }) {
+    subscriptions: {
+        setup ({dispatch}) {
         }
     },
-    effects:{},
-    reducers:{
+    effects: {
+        *login({payload}, {call, put}){
+            yield put({type: 'showLoginButtonLoading'});
+            const data = yield call(login, parse(payload));
+            if (data.success) {
+                yield put({
+                    type: 'loginSuccess', payload: {
+                        user: {
+                            name: payload.username
+                        }
+                    }
+                });
+            }
+            else {
+                yield put({
+                    type: 'loginFail'
+                });
+            }
+        },
+        *queryUser({payload}, {call, put}){
+            const data = yield call(userInfo, parse(payload));
+            if (data.success) {
+                yield put({type: 'loginSuccess', payload: {user: {name: data.username}}});
+            }
+        },
+        *logout({payload}, {call, put}){
+            const data = yield call(logout, parse(payload));
+            if (data.success) {
+                yield put({type: 'logoutSuccess'});
+            }
+
+        }
+    },
+    reducers: {
+        loginSuccess(state, action){
+            return {...state, ...action.payload, login: true, loginButtonLoading: false};
+        },
+        logoutSuccess(state){
+            return {...state, login: false};
+        },
+        loginFail(state){
+            return {...state, login: false, loginButtonLoading: false};
+        },
+        showLoginButtonLoading(state){
+            return {...state, loginButtonLoading: true};
+        }
     }
 };
