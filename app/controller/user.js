a016046ea412074fee935644a828645b15a1bb79
纳兰天忆 <103520146@qq.com>
2017-03-23 10:44:06 +0800

    login logout后端接口

--- app/controller/user.js
+++ app/controller/user.js
@@ -1,17 +1,62 @@
 /**
  * Created by nalantianyi on 2017/3/21.
  */
+let dataKey = [
+    {
+        username: 'guest',
+        password: 'guest',
+    },
+    {
+        username: '纳兰天忆',
+        password: '123456',
+    },
+];
 module.exports = app => {
     class UserController extends app.Controller {
         * userInfo() {
             var session = this.ctx.session;
             const res = {
-                success: session['user_session'],
+                success: session['user_session'] && session['user_session'] > new Date().getTime(),
                 username: session['user_name'] || '',
                 message: ''
             };
             this.ctx.body = res;
         }
+
+        * login() {
+            const userItem = this.ctx.request.body;
+            const response = {success: false, message: ''};
+            const d = dataKey.filter((item) => {
+                return item.username === userItem.username;
+            });
+            if (d.length) {
+                if (d[0].password === userItem.password) {
+                    let session = this.ctx.session;
+                    session['user_session'] = new Date().getTime();
+                    session['user_name'] = userItem.username;
+                    response.message = '登录成功';
+                    response.success = true;
+                }
+                else {
+                    response.message = '密码不正确';
+
+                }
+            }
+            else {
+                response.message = '用户不存在';
+            }
+            this.ctx.body = response;
+        }
+
+        * logout() {
+            this.ctx.session = null;
+            this.ctx.body = {
+                success: true,
+                message: '退出成功'
+            };
+
+        }
+
 
     }
     return UserController;
--- app/router.js
+++ app/router.js
@@ -4,4 +4,7 @@
 module.exports = app => {
     app.get('/', 'home.index');
     app.get('/api/userInfo','user.userInfo');
+    app.post('/api/login','user.login');
+    app.post('/api/logout','user.logout');
+
 };
